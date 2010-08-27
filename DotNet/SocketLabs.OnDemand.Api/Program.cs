287c0dbed5b7b0d096bff9fdbee407f0b0768e32
Nick Berardi <nick@managedfusion.com>
2010-08-27 08:40:12 -0400

    initial commit of examples

---
+++ DotNet/SocketLabs.OnDemand.Api/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Xml.Linq;
+
+namespace SocketLabs.OnDemand.Api
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			int accountId = 1000; // YOUR-ACCOUNT-ID
+			string userName = "YOUR-USER-NAME";
+			string password = "YOUR-API-PASSWORD";
+
+			var apiUri = new Uri("https://api.email-od.com/messagesProcessed?type=xml&accountId=" + accountId);
+			var creds = new NetworkCredential(userName, password);
+			var auth = creds.GetCredential(apiUri, "Basic");
+
+			WebRequest request = WebRequest.Create(apiUri);
+			request.Credentials = auth;
+
+			using (WebResponse response = request.GetResponse())
+			using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+			{
+				XDocument apiResponse = XDocument.Load(reader);
+
+				var addressResponses =
+					from item in apiResponse.Descendants("collection").Descendants("item")
+					select new {
+						ToAddress = item.Element("ToAddress").Value,
+						Response = item.Element("Response").Value
+					};
+
+				foreach (var item in addressResponses)
+				{
+					Console.WriteLine(String.Format("**** (( {0} )) ****", item.ToAddress));
+					Console.WriteLine(item.Response);
+				}
+			}
+
+			Console.Read();
+		}
+	}
+}
---
+++ PHP/od.php
@@ -0,0 +1,30 @@
+<?php
+
+//prints each recipient email addresses associated for delivery failures
+//
+//replace the following constant **** values with your own
+define("ACCOUNT_ID", "9999");
+define("API_USER", "user");
+define("API_PASSWORD", "3150ebe08f4c66a3ba3f");
+
+//calls messagesFailed
+$service_url = 'https://api.email-od.com/messagesFailed?accountId=' . ACCOUNT_ID . '&type=xml';
+$curl = curl_init($service_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERPWD, API_USER . ':' . API_PASSWORD);
+$curl_response = curl_exec($curl);
+curl_close($curl);
+
+//parses XML response and outputs the email addresses of associated with each failure
+$xml = new SimpleXMLElement($curl_response);
+echo $xml->count[0] . ' records returned:<br/>';
+
+echo $xml->collection->item[0]->ToAddress;
+foreach($xml->collection->item as $item) {
+	echo $item->ToAddress . '<br/>';
+}
+print_r($xml);	
+
+//see https://www.socketlabs.com/od/api for complete API documentation
+
+?>
