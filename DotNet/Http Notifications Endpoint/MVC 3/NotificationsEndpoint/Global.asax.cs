811d78d8c1c4ae074027ed14d02222eab27e484a
ScottFigario <Scott.Figario@SocketLabs.com>
2014-11-04 15:46:07 -0500

    updated packages

--- DotNet/Http Notifications Endpoint/MVC 3/NotificationsEndpoint/Global.asax.cs
+++ DotNet/Http Notifications Endpoint/MVC 3/NotificationsEndpoint/Global.asax.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Web;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.Entity;
+//using System.Data.Entity.Infrastructure;
+//using System.Linq;
+//using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
