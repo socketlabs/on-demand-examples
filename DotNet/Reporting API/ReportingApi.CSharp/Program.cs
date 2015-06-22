1ebb26910358d814e3a1e0a2b7881cf648182ac6
ScottFigario <scott.figario@socketlabs.com>
2015-06-22 11:31:33 -0400

    Reworked the ReportingAPI example for C#.  The old example was brittle and wasn't clearly documented.

--- DotNet/Reporting API/ReportingApi.CSharp/Program.cs
+++ DotNet/Reporting API/ReportingApi.CSharp/Program.cs
@@ -1,59 +1,67 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Xml.Linq;
 
 namespace SocketLabs.ReportingApi.CSharp
 {
+
 	internal class Program
 	{
-		private static void Main()
-		{
-			const int serverId = 1000; // YOUR-SERVER-ID
-			const string userName = "YOUR-USER-NAME";
-			const string password = "YOUR-API-PASSWORD";
 
-			var apiUri = new Uri("https://api.socketlabs.com/v1/messagesProcessed?type=xml&serverId=" + serverId);
+        private static void Main()
+        {
+            const int serverId = 1000; // YOUR-SERVER-ID
+            const string userName = "YOUR-USER-NAME";
+            const string password = "YOUR-API-PASSWORD";
+            const string apiUrl = @"https://api.socketlabs.com/v1/";
+            const string apiMethodCall = "accountData"; // accountData, messagesFailed, messagesQueued, messagesProcessed, messagesFblReported, & messagesOpenClick
 
-			WebRequest request = WebRequest.Create(apiUri).WithBasicAuthentication(userName, password);
+            // build the URI
+            var apiUri = new Uri(apiUrl + apiMethodCall + "?type=xml&serverId=" + serverId);
 
-			using (WebResponse response = request.GetResponse())
-			{
-				using (var reader = new StreamReader(response.GetResponseStream()))
-				{
-					XDocument apiResponse = XDocument.Load(reader);
+            // create the request
+            var request = WebRequest.Create(apiUri).WithBasicAuthentication(userName, password);
+        
+            try
+            {
+                // Make the api call
+                using (var response = request.GetResponse())
+                {
 
-					var addressResponses =
-						from item in apiResponse.Descendants("collection").Descendants("item")
-						let xToAddress = item.Element("ToAddress")
-						where xToAddress != null
-						let xResponse = item.Element("Response")
-						where xResponse != null
-						select new
-						       	{
-									ToAddress = xToAddress.Value,
-						       		Response = xResponse.Value
-						       	};
+                    // process the response
+                    var responseData = response.GetResponseStream();
+                    if (responseData == null) return;
 
-					foreach (var item in addressResponses)
-					{
-						Console.WriteLine(String.Format("**** (( {0} )) ****", item.ToAddress));
-						Console.WriteLine(item.Response);
-					}
-				}
-			}
+                    using (var reader = new StreamReader(responseData))
+                    {
+                        // load and display the data
+                        var apiResponse = XDocument.Load(reader);
+                        Console.Write(apiResponse); 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);  // display any error messages
+            }
+            finally
+            {
+                Console.Read(); // keep the console window open 
+            }
 
-			Console.Read();
-		}
+        }
+
 	}
+
+   
 
 	internal static class WebRequestExtensions
 	{
 		public static WebRequest WithBasicAuthentication(this WebRequest req, String userName, String userPassword)
 		{
-			string authInfo = userName + ":" + userPassword;
+			var authInfo = userName + ":" + userPassword;
 			authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
 			req.Headers["Authorization"] = "Basic " + authInfo;
 			return req;
